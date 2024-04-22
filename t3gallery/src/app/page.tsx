5ecbf12975ed43d5625a4c2cfcc8d2a2d950a44a
Muzi <mmzolo@dearx.co.za>
2024-04-22 20:14:52 +0200

    Use Auth for homepage

--- t3gallery/src/app/page.tsx
+++ t3gallery/src/app/page.tsx
@@ -1,26 +1,38 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { mock } from "node:test";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id),
+    
   });
-  console.log(images);  
+  return (
+    <div className="flex flex-wrap gap-4"> {
+      images.map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48 flex flex-col">
+          <img src={image.url}/>
+          <div>{image.name}</div>
+          </div>
+      ))};
+    </div>
+  );
+}
+
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4"> {
-        images.map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 flex flex-col">
-            <img src={image.url}/>
-            <div>{image.name}</div>
-            </div>
-        ))
-      }
-      </div>
+      <SignedOut>
+        <div className="w-full flex flex-col items-center justify-center gap-4 p-4">
+        Please sign in
+        </div>
+        </SignedOut>
+        <SignedIn>
+          <Images/>
+        </SignedIn>
     </main>
   );
 }
