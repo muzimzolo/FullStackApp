5fa9fb0a2bebfe390741160c159f2f7f50ba8d96
Muzi <mmzolo@dearx.co.za>
2024-04-22 14:06:34 +0200

    scafold ui

--- t3gallery/src/app/layout2.tsx
+++ t3gallery/src/app/layout2.tsx
@@ -13,16 +13,6 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-function TopNav() {
-  return (
-    <nav className="flex items-center justify-between w-full border-b p-4 text-x1
-      font-semibolo">
-    <div>Gallery</div>
-    <div>Sign In</div>
-    </nav>
-  );
-}
-
 export default function RootLayout({
   children,
 }: {
@@ -30,10 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-      <TopNav/>
-      {children}
-      </body>
+      <body className={`font-sans ${inter.variable}`}>{children}</body>
     </html>
   );
 }
--- t3gallery/src/app/page.tsx
+++ t3gallery/src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
+import { mock } from "node:test";
+
+const mockUrls = [
+  "https://utfs.io/f/aae2e876-b87f-44f7-a800-4087a8a49da2-1xrfm.png",
+  "https://utfs.io/f/7276993a-0965-4a85-9696-94ee5dde199c-gonerm.png",
+  "https://utfs.io/f/3d7de3b7-69af-48c5-87f6-07f188b8d4e0-lxr74w.png",
+  "https://utfs.io/f/eec224e7-65f2-4e9e-abd1-0aaef05c7db6-mjevyv.png"
+];
+
+const mockImages = mockUrls.map((url, index) =>  ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Default page)
+    <main className="">
+      <div className="flex flex-wrap gap-4"> {
+        mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+            </div>
+        ))
+      }
+      </div>
     </main>
   );
 }
